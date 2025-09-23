a09a6dba17eff6a44f34208f59b76ecd40328359
Md Tausif Bin Mozid <b.tausif15@gamil.com>
2025-09-23 14:16:58 +0600

    Conditonal

---
+++ conditonal_statement.js
@@ -0,0 +1,10 @@
+let age = 20
+if (age < 18) {
+    console.log("You are a minor.");
+} else {
+    console.log("You are an adult.");
+}
+
+if (true) {
+    console.log("Sohana Wins the Debate.");
+}
---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+document.write("This is script.js");
+
+console.log("Script loaded successfully.");
+
+const PI = 3.14159;
+
+function testFun() {
+    let firstName = "John";
+    let lastName = "Doe";
+    console.log(firstName + " " + lastName);
+}
+testFun();
+
+*/
+
+// Data types in JavaScript
+let name = "Alice"; // String
+let age = 30; // Number
+let isStudent = false; // Boolean
+let address; // Undefined
+let phoneNumber = null; // Null
+let hobbies = ["reading", "traveling", "coding"]; // Array
+let person = { firstName: "Alice", lastName: "Smith", age: 30 }; // Object
+let uniqueId = Symbol("id"); // Symbol
+
+console.log(typeof name); // string
+
+console.log(typeof phoneNumber); // object
+console.log(typeof (hobbies)); // object
